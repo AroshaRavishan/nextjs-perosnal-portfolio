47f892b2c5e1a5a22e41ca45c204a470081d4b1b
AroshaRavishan <mgaravishan@gmail.com>
2023-11-23 14:30:30 +0530

    resposive issues

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,12 +4,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+    <div className="w-full fixed top-0 shadow-lg bg-[#03001417] backdrop-blur-md z-50 px-5 md:px-10">
+      <div className="w-full flex items-center justify-between m-auto">
+        <a href="#about-me" className="flex items-center">
           <Image
             src="/NavLogo.png"
             alt="logo"
@@ -18,42 +15,35 @@ const Navbar = () => {
             className="cursor-pointer hover:animate-slowspin"
           />
 
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
+          <span className="font-bold ml-2 hidden md:block text-gray-300">
             Arosha Ravishan
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
-            </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </div>
+        <div className="hidden md:flex items-center justify-between w-1/2 md:w-[500px] border border-[#7042f861] bg-[#0300145e] px-3 py-2 rounded-full text-gray-200">
+          <a href="#about-me" className="cursor-pointer">
+            About me
+          </a>
+          <a href="#skills" className="cursor-pointer">
+            Skills
+          </a>
+          <a href="#projects" className="cursor-pointer">
+            Projects
+          </a>
         </div>
 
-      <div className="flex flex-row gap-5">
-      {Socials.map((social) => (
-        <a
-          href={social.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          key={social.name}
-        >
-          <Image
-            src={social.src}
-            alt={social.name}
-            width={24}
-            height={24}
-          />
-        </a>
-      ))}
-    </div>
+        <div className="flex flex-row gap-5">
+          {Socials.map((social) => (
+            <a
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={social.name}
+            >
+              <Image src={social.src} alt={social.name} width={24} height={24} />
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
